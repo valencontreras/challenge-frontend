e7b858f8ef791765e47a1cad1ef159a7a44ce4c7
Valentina Contreras <valexcontreras@gmail.com>
2025-05-26 03:40:05 -0400

    Feat: create coming soon pages for RTI, OJT and Settings

---
+++ src/app/ojt/page.tsx
@@ -0,0 +1,15 @@
+import { Typography } from "components/common";
+import * as React from "react";
+
+export default function OJTPage() {
+  return (
+    <div className="flex flex-col w-full justify-center items-center h-screen">
+      <Typography type="title" className="text-primary text-center font-bold">
+        OJT
+      </Typography>
+      <Typography type="title" className="text-primary text-center font-bold">
+        Coming Soon...
+      </Typography>
+    </div>
+  );
+}
